6051b581f10672eeb1e434af3a80463d5d56e52a
Lucas Farias <lucas.farias@rtbhouse.com>
2025-01-16 17:45:10 -0300

    feat: define bg color on navigation container routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,17 @@
 import { AuthRoutes } from "@/routes/auth.routes";
-import { NavigationContainer } from "@react-navigation/native";
+import { Box } from "@gluestack-ui/themed";
+import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
+import { gluestackUIConfig } from "config/gluestack-ui.config";
 
 export function Routes() {
+  const theme = DefaultTheme;
+  theme.colors.background = gluestackUIConfig.tokens.colors.gray700;
+
   return (
-    <NavigationContainer>
-      <AuthRoutes />
-    </NavigationContainer>
-  )
+    <Box flex={1} bg="$gray700">
+      <NavigationContainer theme={theme}>
+        <AuthRoutes />
+      </NavigationContainer>
+    </Box>
+  );
 }
