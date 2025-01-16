c7003d2206bfff9fe58d13d5842d99da97c5717e
Lucas Farias <lucas.farias@rtbhouse.com>
2025-01-16 09:46:47 -0300

    feat: create sign-up screen

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -12,7 +12,7 @@ import Logo from "@/assets/logo.svg";
 import { Button } from "@/components/Button";
 import { Input } from "@/components/Input";
 
-export function SignIn() {
+export function SignUp() {
   return (
     <ScrollView
       contentContainerStyle={{ flexGrow: 1 }}
@@ -37,9 +37,10 @@ export function SignIn() {
             </Text>
           </Center>
 
-          <Center gap="$2">
-            <Heading color="$gray100">Acesse a conta</Heading>
+          <Center flex={1} gap="$2">
+            <Heading color="$gray100">Crie sua conta</Heading>
 
+            <Input placeholder="Nome" />
             <Input
               placeholder="E-mail"
               keyboardType="email-address"
@@ -47,16 +48,14 @@ export function SignIn() {
             />
             <Input placeholder="Senha" secureTextEntry />
 
-            <Button title="Acessar" />
+            <Button title="Criar e acessar" />
           </Center>
 
-          <Center flex={1} justifyContent="flex-end" mt="$4">
-            <Text color="$gray100" fontSize="$sm" mb="$3" fontFamily="$body">
-              Ainda não tem acesso?
-            </Text>
-
-            <Button title="Criar Conta" customVariant="outlined" />
-          </Center>
+          <Button
+            title="Voltar para o login"
+            customVariant="outlined"
+            mt="$12"
+          />
         </VStack>
       </VStack>
     </ScrollView>
