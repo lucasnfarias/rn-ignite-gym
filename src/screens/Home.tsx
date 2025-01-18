7c33dd01832468eee2f40d9765687d91e88fca50
Lucas Farias <lucas.farias@rtbhouse.com>
2025-01-18 17:25:14 -0300

    feat: create exercises container on Home

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,16 @@
 import { Group } from "@/components/Group";
 import { HomeHeader } from "@/components/HomeHeader";
-import { VStack } from "@gluestack-ui/themed";
+import { Heading, HStack, Text, VStack } from "@gluestack-ui/themed";
 import { useState } from "react";
 import { FlatList } from "react-native";
 
 export function Home() {
-  const [groups, setGroups] = useState(["Costas", "Bíceps", "Tríceps", "Ombro"]);
+  const [groups, setGroups] = useState([
+    "Costas",
+    "Bíceps",
+    "Tríceps",
+    "Ombro",
+  ]);
   const [groupSelected, setGroupSelected] = useState("Costas");
 
   return (
@@ -27,6 +32,18 @@ export function Home() {
         contentContainerStyle={{ paddingHorizontal: 32 }}
         style={{ marginVertical: 40, maxHeight: 44, minHeight: 44 }}
       />
+
+      <VStack px="$8">
+        <HStack justifyContent="space-between" mb="$5" alignItems="center">
+          <Heading color="$gray200" fontSize="$md" fontFamily="$heading">
+            Exercícios
+          </Heading>
+
+          <Text color="$gray200" fontSize="$sm" fontFamily="$body">
+            4
+          </Text>
+        </HStack>
+      </VStack>
     </VStack>
   );
 }
