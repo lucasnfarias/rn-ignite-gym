fb6943e20765cad74a4a7666c3fbbf63926ca319
Lucas Farias <lucas.farias@rtbhouse.com>
2025-01-18 16:59:39 -0300

    feat: create HomeHeader component

---
+++ src/components/HomeHeader.tsx
@@ -0,0 +1,16 @@
+import { Heading, HStack, Text, VStack } from "@gluestack-ui/themed";
+
+export function HomeHeader() {
+  return (
+    <HStack bg="$gray600" pt="$16" pb="$5" px="$8" alignItems="center">
+      <VStack>
+        <Text color="$gray100" fontSize="$sm">
+          Olá,
+        </Text>
+        <Heading color="$gray100" fontSize="$md">
+          Lucas Farias
+        </Heading>
+      </VStack>
+    </HStack>
+  );
+}
