628332005a2e657197ef906941f28c4b39d04f9b
Lucas Farias <lucas.farias@rtbhouse.com>
2025-01-18 18:09:24 -0300

    feat: improve Input component to be used on Profile

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,9 +1,40 @@
-import { Center, Text } from "@gluestack-ui/themed";
+import { Input } from "@/components/Input";
+import { ScreenHeader } from "@/components/ScreenHeader";
+import { UserPhoto } from "@/components/UserPhoto";
+import { Center, Text, VStack } from "@gluestack-ui/themed";
+import { ScrollView, TouchableOpacity } from "react-native";
 
 export function Profile() {
   return (
-    <Center flex={1}>
-      <Text color="$white">Profile</Text>
-    </Center>
-  )
+    <VStack flex={1}>
+      <ScreenHeader title="Perfil" />
+
+      <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
+        <Center mt="$6" px="$10">
+          <UserPhoto
+            source={{ uri: "https://github.com/lucasnfarias.png" }}
+            alt="imagem do usuário"
+            size="xl"
+          />
+
+          <TouchableOpacity>
+            <Text
+              color="$green500"
+              fontFamily="$heading"
+              fontSize="$md"
+              mt="$2"
+              mb="$8"
+            >
+              Alterar Foto
+            </Text>
+          </TouchableOpacity>
+
+          <Center w="$full" gap="$4">
+            <Input placeholder="Nome" bg="$gray600" />
+            <Input value="lucas@gmail.com" bg="$gray600" isReadOnly />
+          </Center>
+        </Center>
+      </ScrollView>
+    </VStack>
+  );
 }
