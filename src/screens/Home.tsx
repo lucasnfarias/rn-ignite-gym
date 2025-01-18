826d2152e75902899642e378bb2bf1ef8b61adae
Lucas Farias <lucas.farias@rtbhouse.com>
2025-01-18 17:22:56 -0300

    feat: create Groups flat list

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,13 +1,32 @@
 import { Group } from "@/components/Group";
 import { HomeHeader } from "@/components/HomeHeader";
 import { VStack } from "@gluestack-ui/themed";
+import { useState } from "react";
+import { FlatList } from "react-native";
 
 export function Home() {
+  const [groups, setGroups] = useState(["Costas", "Bíceps", "Tríceps", "Ombro"]);
+  const [groupSelected, setGroupSelected] = useState("Costas");
+
   return (
     <VStack flex={1}>
       <HomeHeader />
 
-      <Group name="Costas" isActive={false} />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            isActive={groupSelected === item}
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ paddingHorizontal: 32 }}
+        style={{ marginVertical: 40, maxHeight: 44, minHeight: 44 }}
+      />
     </VStack>
-  )
+  );
 }
